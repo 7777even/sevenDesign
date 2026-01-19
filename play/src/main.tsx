9a0782282f93177deba14253966c2fd4d6c0d90e
7777even <seven.zhong@tec-do.com>
2026-01-19 16:53:49 +0800

    fix:修改样式不生效bug

--- play/src/main.tsx
+++ play/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import '../../packages/components/dist/style.css'
 
 const root = document.getElementById('root')
 if (root) {
